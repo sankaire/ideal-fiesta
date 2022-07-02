cecef29f1a02a221dae0e5414f8b2eb6d63adee8
sankaire <kerenketepela@gmail.com>
2022-07-03 00:57:33 +0300

    fetch data from the query service

---
+++ client/src/CommentList.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+export default ({comments}) =>{
+     const renderedComments =  comments.map(comment =>{
+        return <li key={comment.id}>{comment.content}</li>
+     })
+
+    return <ul>
+        {renderedComments}
+    </ul>
+}
