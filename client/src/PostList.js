b21bc281cc00c91a22e322cfcb118aaddf75f759
sankaire <kerenketepela@gmail.com>
2022-07-03 00:57:10 +0300

    fetch data from the query service

---
+++ client/src/PostList.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import CommentsCreate from "./CommentsCreate";
+import CommentList from "./CommentList";
+export default () => {
+  const [posts, setPosts] = useState({});
+
+  const fetchPosts = async () => {
+    const res = await axios.get("http://localhost:4001/posts");
+    setPosts(res.data);
+  };
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+  const renderedPost = Object.values(posts).map((post) => {
+    return (
+      <div className="card" style={{ width: "30%", marginBottom: "20px" }} key={post.id}>
+        <div className="card-body">
+          <h3>{post.title}</h3>
+          <CommentList comments={post.comments}/>
+          <CommentsCreate postId={post.id}/>
+        </div>
+      </div>
+    );
+  });
+  return <div className="d-flex flex-row flex-wrap justify-content-between" >
+    {renderedPost}
+  </div>;
+};
