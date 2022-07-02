13b672c3cce41abc8dc1689fa31d731759187621
sankaire <kerenketepela@gmail.com>
2022-07-03 01:00:57 +0300

    feat:comment service

--- comments/api/routes/index.routes.js
+++ comments/api/routes/index.routes.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const { createComment, getComments } = require('../../controllers/index.controller');
+const { createComment, getComments, events } = require('../../controllers/index.controller');
 const router = express.Router()
 
 router.route("/posts/:id/comment").post(createComment).get(getComments)
+router.route("/events").post(events)
 
 module.exports = router
