c26c1cf9d823286d5423ab15bb7e413581deff86
sankaire <kerenketepela@gmail.com>
2022-08-12 15:23:28 +0300

    chore:change urls

--- posts/index.js
+++ posts/index.js
@@ -18,7 +18,7 @@ app.post("/posts", async (req, res) => {
     title,
   };
 
-  await axios.post("http://localhost:4005/events", {
+  await axios.post("http://event-bus-srv:4005/events", {
     type: "PostCreated",
     data: {
       id,
@@ -36,5 +36,6 @@ app.post("/events", (req, res) => {
 });
 
 app.listen(4000, () => {
+  console.log('v55')
   console.log("Listening on 4000");
 });
